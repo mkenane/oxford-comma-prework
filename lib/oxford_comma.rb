509e3ef72a6d578f4d9ee76014d9a442feed9809
Mor Kenane <morkenan@hotmail.com>
2018-02-17 00:10:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.join("")
+  elsif array.length == 2 
+  return array.join(" and ")
+ else 
+  last_two = array.pop(2)
+  to_string = array.join(", ")
+  return "#{to_string}, #{last_two[0]}, and #{last_two[1]}"
+  end 
 end
