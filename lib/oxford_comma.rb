bd270f08096a5342f89145d396d4b5ac7d6488ab
Mor Kenane <morkenan@hotmail.com>
2018-02-17 00:09:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 
     return array.join("")
-  
   elsif array.length == 2 
   return array.join(" and ")
- 
  else 
   last_two = array.pop(2)
   to_string = array.join(", ")
